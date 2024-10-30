ade5cbb5bfd1b6031eff98658cde4cb81f15e1cc
Osama <osamaalhyari99@gmail.com>
2024-10-30 21:31:40 +0300

    ..

--- Server/database/seeders/GroupSeeder.php
+++ Server/database/seeders/GroupSeeder.php
@@ -14,17 +14,17 @@ class GroupSeeder extends Seeder
     public function run(): void
     {
         //
-        DB::table('users')->insert([
+        DB::table('groups')->insert([
             "id" => 1,
             'name' => "Group 1",
             'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus a nisi est rerum quaerat animi accusamus quos saepe fugiat ullam, expedita sed, commodi doloremque autem quis dolores at consequatur accusantium!',
         ]);
-        DB::table('users')->insert([
+        DB::table('groups')->insert([
             "id" => 2,
             'name' => "Group 2",
             'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus a nisi est rerum quaerat animi accusamus quos saepe fugiat ullam, expedita sed, commodi doloremque autem quis dolores at consequatur accusantium!',
         ]);
-        DB::table('users')->insert([
+        DB::table('groups')->insert([
             "id" => 3,
             'name' => "Group 3",
             'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus a nisi est rerum quaerat animi accusamus quos saepe fugiat ullam, expedita sed, commodi doloremque autem quis dolores at consequatur accusantium!',
